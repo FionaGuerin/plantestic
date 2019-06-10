39f4d91f1101ddf72b1f3a74017517db49eb99f4
Fiona Guerin <fiona.guerin@tum.de>
2019-06-10 20:32:24 +0200

    Research how Cucumber and Rest-assured play together.

---
+++ src/test/kotlin/FunctionalTest.kt
@@ -0,0 +1,29 @@
+import io.restassured.RestAssured
+import io.restassured.specification.RequestSpecification
+import org.junit.BeforeClass
+
+open class FunctionalTest {
+
+    companion object {
+
+        @BeforeClass @JvmStatic
+        fun setUp() {
+            startWireMock()
+            setupRestAssuredDefaults()
+        }
+
+        private fun startWireMock() {
+            Runtime.getRuntime().exec("java -jar wiremock-standalone-2.19.0.jar --port 8888 --root-dir src/test/resources")
+        }
+
+        private fun setupRestAssuredDefaults() {
+            RestAssured.baseURI = "http://localhost"
+            RestAssured.port = 8888
+            RestAssured.basePath = "/"
+        }
+    }
+}
+
+fun RequestSpecification.When(): RequestSpecification {
+    return this.`when`()
+}
