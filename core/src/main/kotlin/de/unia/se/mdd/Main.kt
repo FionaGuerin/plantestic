8cb3977d16128ed72bd8cc5e24be59e5335082a1
Andreas Zimmerer <andreas.zimmerer@tum.de>
2019-06-28 22:56:57 +0200

    MVP: pipeline with tmp file #39

--- core/src/main/kotlin/de/unia/se/mdd/Main.kt
+++ core/src/main/kotlin/de/unia/se/mdd/Main.kt
@@ -1,46 +1,28 @@
 package de.unia.se.mdd
 
 import com.google.common.io.Resources
-import java.io.StringWriter
-import java.io.IOException
 import org.eclipse.emf.ecore.*
 import org.eclipse.emf.ecore.resource.Resource
 import org.eclipse.emf.ecore.resource.ResourceSet
 import org.eclipse.emf.ecore.resource.URIConverter.WriteableOutputStream
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl
-import org.eclipse.emf.ecore.util.BasicExtendedMetaData
-import org.eclipse.emf.ecore.xmi.XMLResource
 import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl
 import org.eclipse.m2m.qvt.oml.ExecutionContextImpl
 import org.eclipse.m2m.qvt.oml.TransformationExecutor
-import plantuml.PumlRuntimeModule
-import org.eclipse.core.runtime.IStatus
 import org.eclipse.emf.common.util.*
-import org.eclipse.m2m.qvt.oml.ExecutionDiagnostic
 import org.eclipse.m2m.qvt.oml.BasicModelExtent
-import org.eclipse.m2m.qvt.oml.ModelExtent
 import plantuml.PumlStandaloneSetup
 import plantuml.PumlStandaloneSetupGenerated
 import plantuml.puml.UmlDiagram
-import java.util.*
 import kotlin.collections.HashMap
 import org.eclipse.emf.common.util.URI.createFileURI
-import org.eclipse.emf.common.util.URI.createFileURI
-import org.eclipse.emf.ecore.impl.BasicEObjectImpl
-import org.eclipse.emf.ecore.impl.DynamicEObjectImpl
-import org.eclipse.emf.ecore.impl.EObjectImpl
-import org.eclipse.emf.ecore.resource.URIConverter
-import org.eclipse.emf.ecore.util.EContentsEList
-import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl
-import org.eclipse.m2m.internal.qvt.oml.emf.util.EmfUtil.createResource
-import org.eclipse.m2m.qvt.oml.util.Log
 import org.eclipse.m2m.qvt.oml.util.WriterLog
 import org.eclipse.uml2.uml.UMLPackage
 import org.eclipse.uml2.uml.resource.UMLResource
 import plantuml.puml.PumlPackage
-import java.io.File
-import java.io.OutputStreamWriter
+import java.io.*
 import java.lang.IllegalArgumentException
+import java.io.IOException
 
 
 object Main {
@@ -78,37 +60,8 @@ object Main {
         val rs = ResourceSetImpl()
 
 
-        // TODO register factories
+        // register encoding factories
         PumlStandaloneSetup.doSetup()
-        //rs.resourceFactoryRegistry.extensionToFactoryMap[packageInstance.nsURI] = packageInstance
-        //rs.resourceFactoryRegistry.extensionToFactoryMap["xmi"] = XMIResourceFactoryImpl()
-        //rs.resourceFactoryRegistry.extensionToFactoryMap["ecore"] = EcoreResourceFactoryImpl()
-
-        // TODO load input model
-        val INPUT_URI = createFileURI(Resources.getResource("minimal_hello.puml").path)
-        val inputModel = rs.getResource(INPUT_URI, true)
-
-        val rs_temp = ResourceSetImpl()
-        val inputResource = rs_temp.createResource(URI.createURI("dummy:/test.ecore"))
-        //inputResource.contents.add(inputModel.contents[0])
-
-
-        //inputModel.uri = URI.createURI("dummy:/test.ecore")
-
-
-        //val test1 = inputModel.getEObject("dummy:test.ecore")
-
-        /*val stringWriter = StringWriter()
-        val outputStream = URIConverter.WriteableOutputStream(
-            stringWriter,
-            "UTF-8"
-        )
-        val options = HashMap<String, String>()
-        inputResource.save(outputStream, options)
-
-        println(stringWriter.toString())
-*/
-
 
 
         // FIXME Not sure why we need to do this?
@@ -137,20 +90,27 @@ object Main {
         val validationDiagnostic = executor.loadTransformation()
         require(validationDiagnostic.message == "OK")
 
-        val examle_xmi = createFileURI(Resources.getResource("diagram.xmi").path)
-        val input_xmi = rs.getResource(examle_xmi, true)
 
-        //val inObjects = EContentsEList<EObject>(diagram)
-        //inObjects.add(diagram)
+        // load input model and parse it, then write it back to a temp file in ecore format (xmi)
+        // then load it back by parsing xmi (this somehow adds eSttings which is still a WIP)
+        val INPUT_URI = createFileURI(Resources.getResource("minimal_hello.puml").path)
+        val inputModel = rs.getResource(INPUT_URI, true)
 
+        val tmpFileResource = rs.createResource(URI.createURI("dummy:/test.ecore"))
+        tmpFileResource.contents.addAll(inputModel.contents)
+
+        val fileTmpOutput = FileOutputStream("tmp.xmi")
+        val tmpFileOptions = HashMap<String, String>()
+        tmpFileResource.save(fileTmpOutput, tmpFileOptions)
+
+        val tmpFileUri = createFileURI("tmp.xmi")
+        val inputModelEcore = rs.getResource(tmpFileUri, true)
 
 
         // create the input extent with its initial contents
-        val input_1 = BasicModelExtent(inputModel.contents.map { d -> DynamicEObjectImpl(d.eClass()) })
-        val input_2 = BasicModelExtent(input_xmi.contents)
+        val input = BasicModelExtent(inputModelEcore.contents)
         // create an empty extent to catch the output
         val output = BasicModelExtent()
-
 
 
         // setup the execution environment details ->
@@ -165,33 +125,25 @@ object Main {
         // run the transformation assigned to the executor with the given
         // input and output and execution context -> ChangeTheWorld(in, out)
         // Remark: variable arguments count is supported
-        val result = executor.execute(context, input_2, output)
+        val result = executor.execute(context, input, output)
 
+
+        // now delete the tmp file
+        File("tmp.xmi").delete()
 
 
         // check the result for success
         if (result.severity == Diagnostic.OK) {
-            // the output objects got captured in the output extent
-            val outObjects = output.contents
-            // let's persist them using a resource
-            val outputLocationString = Resources.getResource("qvt").path + "/result.uml"
-            val outputPathUri = URI.createURI("file:$outputLocationString")
-            val file = File(outputLocationString)
-            file.createNewFile()
-
-
-
-            EPackage.Registry.INSTANCE[UMLPackage.eNS_URI] = UMLPackage.eINSTANCE
-            Resource.Factory.Registry.INSTANCE.extensionToFactoryMap[UMLResource.FILE_EXTENSION] =
-                UMLResource.Factory.INSTANCE
-
-            val outResource = rs.createResource(outputPathUri) as UMLResource
-            outResource.contents.addAll(outObjects)
+            val fileOutput = FileOutputStream("out.uml")
+            val options = HashMap<String, String>()
+            val outResource = rs.createResource(URI.createURI("dummy:/out.uml")) as UMLResource
+            outResource.contents.addAll(output.contents)
             try {
-                outResource.save(null)
+                outResource.save(fileOutput, options)
             } catch (e: IOException) {
                 e.printStackTrace()
             }
+
 
         } else {
             // turn the result diagnostic into status and send it to error log
@@ -199,44 +151,5 @@ object Main {
             throw IllegalArgumentException(result.toString())
             //TODO Activator.getDefault().getLog().log(status)
         }
-    }
-
-    fun getEcoreLectureExample(): String {
-        val ecoreFactory: EcoreFactory = EcoreFactory.eINSTANCE
-        val aPackage: EPackage = ecoreFactory.createEPackage()
-
-        aPackage.name = "somePackage"
-        aPackage.nsPrefix = "pkg"
-        aPackage.nsURI = "urn:www.pst.ifi.lmu.de/knapp/pkg"
-        val aClass: EClass = ecoreFactory.createEClass()
-        aClass.name = "SomeClass"
-        aPackage.eClassifiers.add(aClass)
-        val anAttribute: EAttribute = ecoreFactory.createEAttribute()
-        anAttribute.name = "someAttribute"
-        anAttribute.eType = ecoreFactory.ecorePackage.eString
-        aClass.eStructuralFeatures.add(anAttribute)
-        val aReference: EReference = ecoreFactory.createEReference()
-        aReference.name = "someReference"
-        aReference.eType = aClass
-        aClass.eStructuralFeatures.add(aReference)
-
-        val stringWriter = StringWriter()
-        try {
-            Resource.Factory.Registry.INSTANCE.extensionToFactoryMap["ecore"] = EcoreResourceFactoryImpl()
-            val resourceSet: ResourceSet = ResourceSetImpl()
-            val resource: Resource = resourceSet.createResource(URI.createFileURI("test.ecore"))
-            resource.contents.add(aPackage)
-
-            val outputStream = WriteableOutputStream(
-                stringWriter,
-                "UTF-8"
-            )
-            val options: Map<String, String> = HashMap()
-            resource.save(outputStream, options)
-        } catch (ioe: IOException) {
-            ioe.printStackTrace()
-        }
-
-        return stringWriter.toString()
     }
 }
--- core/src/test/kotlin/de/unia/se/mdd/MainTest.kt
+++ core/src/test/kotlin/de/unia/se/mdd/MainTest.kt
@@ -1,25 +1,9 @@
 package de.unia.se.mdd
 
-import com.google.common.io.Resources
-import de.unia.se.mdd.Main.getEcoreLectureExample
 import de.unia.se.mdd.Main.transformPuml2ReqRes
-import io.kotlintest.matchers.numerics.shouldBeGreaterThan
-import io.kotlintest.matchers.startWith
-import io.kotlintest.matchers.string.shouldContain
-import io.kotlintest.should
 import io.kotlintest.specs.StringSpec
 
 class MainTest : StringSpec({
-
-    "Lecture example should contain expected xml tags" {
-        val lectureExample = getEcoreLectureExample()
-        lectureExample should startWith("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
-        lectureExample.shouldContain("ecore:EPackage")
-    }
-
-    "Lecture example should be longer than a threshold" {
-        getEcoreLectureExample().length.shouldBeGreaterThan(500)
-    }
 
     "Bullshit transformation throws error" {
         //val parser = PumlParser()
@@ -27,4 +11,5 @@ class MainTest : StringSpec({
 
         transformPuml2ReqRes()
     }
+
 })
