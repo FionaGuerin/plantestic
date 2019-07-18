fea9cf15d99a18a78f151d234745ac88bda9386f
Andreas Zimmerer <andreas.zimmerer@tum.de>
2019-07-18 15:00:39 +0200

    do not load UML; resolve all cross references in PUML #71

--- core/src/test/kotlin/de/unia/se/mdd/PumlParserTest.kt
+++ core/src/test/kotlin/de/unia/se/mdd/PumlParserTest.kt
@@ -3,6 +3,9 @@ package de.unia.se.mdd
 import com.google.common.io.Resources
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
+import org.eclipse.emf.common.util.URI
+import org.eclipse.emf.ecore.EObject
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl
 import plantuml.puml.Participant
 import plantuml.puml.SequenceUml
 
@@ -11,7 +14,9 @@ class PumlParserTest : StringSpec({
     "Minimal example can be parsed from URI" {
         MetaModelSetup.doSetup()
 
-        val umlDiagram = PumlParser.parse(Resources.getResource("minimal_hello.puml").path)
+        val umlDiagram = PumlParser.parse(PUML_INPUT_URI_STRING)
+
+        printModel(umlDiagram)
 
         (umlDiagram.umlDiagrams[0] is SequenceUml) shouldBe true
         val sequenceDiagram = umlDiagram.umlDiagrams[0] as SequenceUml
@@ -19,4 +24,15 @@ class PumlParserTest : StringSpec({
         (sequenceDiagram.umlElements[0] as Participant).name shouldBe "A"
         (sequenceDiagram.umlElements[1] as Participant).name shouldBe "B"
     }
-})
+}) {
+    companion object {
+        private val PUML_INPUT_URI_STRING = Resources.getResource("minimal_hello.puml").path
+
+        fun printModel(model: EObject) {
+            val resource = ResourceSetImpl().createResource(URI.createURI("dummy:/test.ecore"))
+            resource.contents.add(model)
+
+            resource.save(System.out, null)
+        }
+    }
+}
