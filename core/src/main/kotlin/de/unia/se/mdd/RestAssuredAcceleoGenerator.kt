aca17ee119781a6b2a53c17eafa28edf0d817771
stefan-grafberger <stefangrafberger@gmail.com>
2019-07-18 19:27:24 +0200

    More cleanup

--- core/src/main/kotlin/de/unia/se/mdd/RestAssuredAcceleoGenerator.kt
+++ core/src/main/kotlin/de/unia/se/mdd/RestAssuredAcceleoGenerator.kt
@@ -5,7 +5,6 @@ import org.eclipse.acceleo.engine.service.AbstractAcceleoGenerator
 import org.eclipse.acceleo.parser.compiler.AcceleoCompiler
 import org.eclipse.emf.common.util.BasicMonitor
 import org.eclipse.emf.common.util.Monitor
-import org.eclipse.emf.common.util.URI
 import org.eclipse.emf.ecore.EObject
 import java.io.File
 import java.io.IOException
@@ -14,26 +13,26 @@ class RestAssuredAcceleoGenerator : AbstractAcceleoGenerator() {
 
     private val moduleName = "generateCode.mtl"
     private val templateNames = arrayOf("generateTestScenario")
-    private val inputFolder =
-        URI.createFileURI(Resources.getResource("code-generation").path)
-    private val outputFolder = System.getProperty("user.dir") + "/build/classes/kotlin/main/de/unia/se/mdd/"
-
-    @Throws(IOException::class)
-    override fun doGenerate(monitor: Monitor) {
-        println("Generating code from Rest assured Model")
-        super.doGenerate(BasicMonitor())
-    }
+    private val acceleoTransformationsInputFolder = Resources.getResource("code-generation").path
+    private val compiledAcceleoTransformationsOutputFolder = System.getProperty("user.dir") +
+            "/build/classes/kotlin/main/de/unia/se/mdd/"
 
     override fun initialize(model: EObject, targetFolder: File, arguments: List<Any>) {
         println("Compiling .mtl file for generating code from Rest assured Model")
         val acceleoCompiler = AcceleoCompiler()
 
-        acceleoCompiler.setSourceFolder(inputFolder.path())
-        acceleoCompiler.setOutputFolder(outputFolder)
+        acceleoCompiler.setSourceFolder(acceleoTransformationsInputFolder)
+        acceleoCompiler.setOutputFolder(compiledAcceleoTransformationsOutputFolder)
         acceleoCompiler.setBinaryResource(false)
         acceleoCompiler.execute()
 
         super.initialize(model, targetFolder, arguments)
+    }
+
+    @Throws(IOException::class)
+    override fun doGenerate(monitor: Monitor) {
+        println("Generating code from Rest assured Model")
+        super.doGenerate(BasicMonitor())
     }
 
     override fun getModuleName(): String {
