54a6ace75266ad62d4c07e810bc4ce3b443b7883
Andreas Zimmerer <andreas.zimmerer@tum.de>
2019-07-20 00:50:09 +0200

    compile output of acceleo transformation and execute it #59

--- core/src/test/kotlin/de/unia/se/mdd/End2EndTest.kt
+++ core/src/test/kotlin/de/unia/se/mdd/End2EndTest.kt
@@ -3,10 +3,25 @@ package de.unia.se.mdd
 import com.google.common.io.Resources
 import de.unia.se.mdd.Main.runTransformationPipeline
 import io.kotlintest.specs.StringSpec
+import org.joor.Reflect
+import java.io.File
+
+interface CompiledTest {
+    fun test()
+}
 
 class End2EndTest : StringSpec({
 
     "End2End test does not fail" {
-        runTransformationPipeline(Resources.getResource("minimal_hello.puml").path)
+        runTransformationPipeline(INPUT_PATH, OUTPUT_PATH)
+
+        // Now compile the resulting code and execute it
+        val compiledTest : CompiledTest = Reflect.compile("com.mdd.test.Test", File(OUTPUT_PATH + "/TestName.java").readText()).create().get()
+        println(compiledTest.test())
     }
-})
+}) {
+    companion object {
+        private val INPUT_PATH = Resources.getResource("minimal_hello.puml").path
+        private val OUTPUT_PATH = Resources.getResource("code-generation").path + "/generatedCode"
+    }
+}
