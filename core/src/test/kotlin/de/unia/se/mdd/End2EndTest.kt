8bcd2509b7841be90d6235db0d944d8165d9cc83
MichelleMar <michelle.martin17696@gmail.com>
2019-07-21 22:39:29 +0200

    add two more toml files, add them to End2End test

--- core/src/test/kotlin/de/unia/se/mdd/End2EndTest.kt
+++ core/src/test/kotlin/de/unia/se/mdd/End2EndTest.kt
@@ -22,7 +22,7 @@ class End2EndTest : StringSpec({
 
         // Now compile the resulting code
         Reflect.compile("com.mdd.test.Test", File("$OUTPUT_PATH/testScenario.java").readText())
-            .create(CONFIG_PATH)
+            .create(MINIMAL_EXAMPLE_CONFIG_PATH)
     }
 
     "End2End test receives request on mock server for the minimal example".config(enabled = false) {
@@ -32,7 +32,7 @@ class End2EndTest : StringSpec({
 
         // Now compile the resulting code and execute it
         val compiledTest = Reflect.compile("com.mdd.test.Test", File("$OUTPUT_PATH/testScenario.java").readText())
-            .create(CONFIG_PATH)
+            .create(MINIMAL_EXAMPLE_CONFIG_PATH)
         compiledTest.call("test")
 
         // Check if we received a correct request
@@ -43,18 +43,29 @@ class End2EndTest : StringSpec({
 
     "End2End works for the rerouting example".config(enabled = true) {
         runTransformationPipeline(REROUTE_INPUT_PATH)
+
+        // Now compile the resulting code
+        Reflect.compile("com.mdd.test.Test", File("$OUTPUT_PATH/testScenario.java").readText())
+            .create(REROUTE_CONFIG_PATH)
     }
 
     "End2End test works for the xcall example".config(enabled = true) {
         runTransformationPipeline(XCALL_INPUT_PATH)
+
+        // Now compile the resulting code
+        Reflect.compile("com.mdd.test.Test", File("$OUTPUT_PATH/testScenario.java").readText())
+            .create(XCALL_CONFIG_PATH)
     }
 }) {
     companion object {
         private val MINIMAL_EXAMPLE_INPUT_PATH = Resources.getResource("minimal_hello.puml").path
-        private val CONFIG_PATH = Resources.getResource("end2end_test_config_minimal_hello.toml").path
+        private val MINIMAL_EXAMPLE_CONFIG_PATH = Resources.getResource("end2end_test_config_minimal_hello.toml").path
 
         private val REROUTE_INPUT_PATH = Resources.getResource("rerouting.puml").path
+        private val REROUTE_CONFIG_PATH = Resources.getResource("end2end_test_config_minimal_hello.toml").path
+
         private val XCALL_INPUT_PATH = Resources.getResource("xcall.puml").path
+        private val XCALL_CONFIG_PATH = Resources.getResource("end2end_test_config_minimal_hello.toml").path
 
         private val OUTPUT_PATH = Resources.getResource("code-generation").path + "/generatedCode"
     }
