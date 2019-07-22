823ea0c54d4875764a2096ea566ef05f54f63761
MichelleMar <michelle.martin17696@gmail.com>
2019-07-22 19:49:41 +0200

    add more tests; currently disabled because XMI input is currently missing

--- core/src/test/kotlin/de/unia/se/mdd/AcceleoGeneratorTest.kt
+++ core/src/test/kotlin/de/unia/se/mdd/AcceleoGeneratorTest.kt
@@ -11,7 +11,7 @@ import org.joor.Reflect
 import java.io.File
 
 class AcceleoGeneratorTest : StringSpec({
-    "Transform a Rest Assured EObject input to Java Code".config(enabled = true) {
+    "Transform a Rest Assured EObject input to Java Code for minimal hello".config(enabled = true) {
         MetaModelSetup.doSetup()
 
         val pumlInputModelURI = URI.createFileURI(MINIMAL_EXAMPLE_INPUT_PATH)
@@ -24,7 +24,7 @@ class AcceleoGeneratorTest : StringSpec({
         printCode(outputFolder)
     }
 
-    "Acceleo generation produces valid Java code".config(enabled = true) {
+    "Acceleo generation produces valid Java code for minimal example".config(enabled = true) {
         MetaModelSetup.doSetup()
 
         val pumlInputModelURI = URI.createFileURI(MINIMAL_EXAMPLE_INPUT_PATH)
@@ -57,10 +57,113 @@ class AcceleoGeneratorTest : StringSpec({
         wireMockServer.allServeEvents[0].response.status shouldBe 200
         wireMockServer.allServeEvents.forEach { serveEvent -> println(serveEvent.request) }
     }
+
+    "Transform a Rest Assured EObject input to Java Code for rerouting".config(enabled = false) {
+        MetaModelSetup.doSetup()
+
+        val pumlInputModelURI = URI.createFileURI(REROUTING_INPUT_PATH)
+        val pumlInputModel = ResourceSetImpl().getResource(pumlInputModelURI, true).contents[0]
+        val outputFolder = File(OUTPUT_PATH)
+
+        AcceleoCodeGenerator.generateCode(pumlInputModel, outputFolder)
+        outputFolder.listFiles().size.shouldBeGreaterThanOrEqual(1)
+
+        printCode(outputFolder)
+    }
+
+    "Acceleo generation produces valid Java code for rerouting".config(enabled = false) {
+        MetaModelSetup.doSetup()
+
+        val pumlInputModelURI = URI.createFileURI(REROUTING_INPUT_PATH)
+        val pumlInputModel = ResourceSetImpl().getResource(pumlInputModelURI, true).contents[0]
+        val outputFolder = File(OUTPUT_PATH)
+
+        AcceleoCodeGenerator.generateCode(pumlInputModel, outputFolder)
+
+        // Now compile the resulting code
+        Reflect.compile("com.mdd.test.Test", File(OUTPUT_PATH + "/scenario.java").readText()).create(REROUTING_CONFIG_PATH)
+    }
+
+    "Acceleo generation test receives request on mock server for rerouting".config(enabled = false) {
+        wireMockServer.stubFor(WireMock.get(WireMock.urlEqualTo("/hello/123")).willReturn(WireMock.aResponse().withBody("test")))
+
+        MetaModelSetup.doSetup()
+
+        val pumlInputModelURI = URI.createFileURI(REROUTING_INPUT_PATH)
+        val pumlInputModel = ResourceSetImpl().getResource(pumlInputModelURI, true).contents[0]
+        val outputFolder = File(OUTPUT_PATH)
+
+        AcceleoCodeGenerator.generateCode(pumlInputModel, outputFolder)
+
+        // Now compile the resulting code and execute it
+        val compiledTest = Reflect.compile("com.mdd.test.Test", File(OUTPUT_PATH + "/scenario.java").readText()).create(REROUTING_CONFIG_PATH)
+        compiledTest.call("test")
+
+        // Check if we received a correct request
+        wireMockServer.allServeEvents.size shouldBe 1
+        wireMockServer.allServeEvents[0].response.status shouldBe 200
+        wireMockServer.allServeEvents.forEach { serveEvent -> println(serveEvent.request) }
+    }
+
+    "Transform a Rest Assured EObject input to Java Code for xcall".config(enabled = false) {
+        MetaModelSetup.doSetup()
+
+        val pumlInputModelURI = URI.createFileURI(XCALL_INPUT_PATH)
+        val pumlInputModel = ResourceSetImpl().getResource(pumlInputModelURI, true).contents[0]
+        val outputFolder = File(OUTPUT_PATH)
+
+        AcceleoCodeGenerator.generateCode(pumlInputModel, outputFolder)
+        outputFolder.listFiles().size.shouldBeGreaterThanOrEqual(1)
+
+        printCode(outputFolder)
+    }
+
+    "Acceleo generation produces valid Java code for xcall".config(enabled = false) {
+        MetaModelSetup.doSetup()
+
+        val pumlInputModelURI = URI.createFileURI(XCALL_INPUT_PATH)
+        val pumlInputModel = ResourceSetImpl().getResource(pumlInputModelURI, true).contents[0]
+        val outputFolder = File(OUTPUT_PATH)
+
+        AcceleoCodeGenerator.generateCode(pumlInputModel, outputFolder)
+
+        // Now compile the resulting code
+        Reflect.compile("com.mdd.test.Test", File(OUTPUT_PATH + "/scenario.java").readText()).create(XCALL_CONFIG_PATH)
+    }
+
+    "Acceleo generation test receives request on mock server for the xcall".config(enabled = false) {
+        wireMockServer.stubFor(WireMock.get(WireMock.urlEqualTo("/hello/123")).willReturn(WireMock.aResponse().withBody("test")))
+
+        MetaModelSetup.doSetup()
+
+        val pumlInputModelURI = URI.createFileURI(XCALL_INPUT_PATH)
+        val pumlInputModel = ResourceSetImpl().getResource(pumlInputModelURI, true).contents[0]
+        val outputFolder = File(OUTPUT_PATH)
+
+        AcceleoCodeGenerator.generateCode(pumlInputModel, outputFolder)
+
+        // Now compile the resulting code and execute it
+        val compiledTest = Reflect.compile("com.mdd.test.Test", File(OUTPUT_PATH + "/scenario.java").readText()).create(XCALL_CONFIG_PATH)
+        compiledTest.call("test")
+
+        // Check if we received a correct request
+        wireMockServer.allServeEvents.size shouldBe 1
+        wireMockServer.allServeEvents[0].response.status shouldBe 200
+        wireMockServer.allServeEvents.forEach { serveEvent -> println(serveEvent.request) }
+    }
 }) {
     companion object {
         private val MINIMAL_EXAMPLE_INPUT_PATH = Resources.getResource("minimal_hello_restassured.xmi").path
         private val MINIMAL_EXAMPLE_CONFIG_PATH = Resources.getResource("end2end_test_config_minimal_hello.toml").path
+
+        //TODO: set correct path to xmi file
+        private val REROUTING_INPUT_PATH = Resources.getResource("minimal_hello_restassured.xmi").path
+        private val REROUTING_CONFIG_PATH = Resources.getResource("end2end_test_config_rerouting.toml").path
+
+        //TODO: set correct path to xmi-file
+        private val XCALL_INPUT_PATH = Resources.getResource("minimal_hello_restassured.xmi").path
+        private val XCALL_CONFIG_PATH = Resources.getResource("end2end_test_config_xcall.toml").path
+
         private val OUTPUT_PATH = Resources.getResource("code-generation").path + "/generatedCode"
 
         fun printCode(folder: File) {
