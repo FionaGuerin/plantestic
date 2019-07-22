c11a8bc1afee617a11c585cdce1598a55ff052db
MichelleMar <michelle.martin17696@gmail.com>
2019-07-22 09:19:55 +0200

    remove unsused paramter OUTPUT_PATH; add compilation of xcall and rerouting end2end test; add correct config to toml files

--- core/src/test/kotlin/de/unia/se/mdd/End2EndTest.kt
+++ core/src/test/kotlin/de/unia/se/mdd/End2EndTest.kt
@@ -17,22 +17,22 @@ val wireMockServer = WireMockServer(8080)
 
 class End2EndTest : StringSpec({
 
-    "End2End test produces valid Java code for the minimal example".config(enabled = false) {
-        runTransformationPipeline(MINIMAL_EXAMPLE_INPUT_PATH, OUTPUT_PATH)
+    "End2End test produces valid Java code for the minimal example".config(enabled = true) {
+        runTransformationPipeline(MINIMAL_EXAMPLE_INPUT_PATH)
 
         // Now compile the resulting code
         Reflect.compile("com.mdd.test.Test", File("$OUTPUT_PATH/testScenario.java").readText())
-            .create(CONFIG_PATH)
+            .create(MINIMAL_EXAMPLE_CONFIG_PATH)
     }
 
     "End2End test receives request on mock server for the minimal example".config(enabled = false) {
         wireMockServer.stubFor(get(urlEqualTo("/hello/123")).willReturn(aResponse().withBody("test")))
 
-        runTransformationPipeline(MINIMAL_EXAMPLE_INPUT_PATH, OUTPUT_PATH)
+        runTransformationPipeline(MINIMAL_EXAMPLE_INPUT_PATH)
 
         // Now compile the resulting code and execute it
         val compiledTest = Reflect.compile("com.mdd.test.Test", File("$OUTPUT_PATH/testScenario.java").readText())
-            .create(CONFIG_PATH)
+            .create(MINIMAL_EXAMPLE_CONFIG_PATH)
         compiledTest.call("test")
 
         // Check if we received a correct request
@@ -41,20 +41,31 @@ class End2EndTest : StringSpec({
         wireMockServer.allServeEvents.forEach { serveEvent -> println(serveEvent.request) }
     }
 
-    "End2End works for the rerouting example".config(enabled = false) {
-        runTransformationPipeline(REROUTE_INPUT_PATH, OUTPUT_PATH)
+    "End2End works for the rerouting example".config(enabled = true) {
+        runTransformationPipeline(REROUTE_INPUT_PATH)
+
+        // Now compile the resulting code
+        Reflect.compile("com.mdd.test.Test", File("$OUTPUT_PATH/testScenario.java").readText())
+            .create(REROUTE_CONFIG_PATH)
     }
 
-    "End2End test works for the xcall example".config(enabled = false) {
-        runTransformationPipeline(XCALL_INPUT_PATH, OUTPUT_PATH)
+    "End2End test works for the xcall example".config(enabled = true) {
+        runTransformationPipeline(XCALL_INPUT_PATH)
+
+        // Now compile the resulting code
+        Reflect.compile("com.mdd.test.Test", File("$OUTPUT_PATH/testScenario.java").readText())
+            .create(XCALL_CONFIG_PATH)
     }
 }) {
     companion object {
         private val MINIMAL_EXAMPLE_INPUT_PATH = Resources.getResource("minimal_hello.puml").path
-        private val CONFIG_PATH = Resources.getResource("end2end_test_config_minimal_hello.toml").path
+        private val MINIMAL_EXAMPLE_CONFIG_PATH = Resources.getResource("end2end_test_config_minimal_hello.toml").path
 
         private val REROUTE_INPUT_PATH = Resources.getResource("rerouting.puml").path
+        private val REROUTE_CONFIG_PATH = Resources.getResource("end2end_test_config_minimal_hello.toml").path
+
         private val XCALL_INPUT_PATH = Resources.getResource("xcall.puml").path
+        private val XCALL_CONFIG_PATH = Resources.getResource("end2end_test_config_minimal_hello.toml").path
 
         private val OUTPUT_PATH = Resources.getResource("code-generation").path + "/generatedCode"
     }
